ff96ba685478b0cd21240b321d26aa05922fa6a0
marcusp619 <marcusp619@gmail.com>
2019-01-14 18:00:39 -0700

    Remove unused code

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { simpleAction } from '../../actions/simpleAction'
-import logo from './logo.svg';
+import { simpleAction } from '../../actions/simpleAction';
 import './App.css';
 
 class App extends Component {
@@ -9,24 +8,10 @@ class App extends Component {
   simpleAction = (event) => {
     this.props.simpleAction();
   }
-  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
         <button onClick={this.simpleAction}>Test redux action</button>
         <pre>
           {
