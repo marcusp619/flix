ce3b6face8d4532a9989da43ecd553387f559e2d
marcusp619 <marcusp619@gmail.com>
2019-01-19 23:14:22 -0700

    Add movies reducer

--- src/reducers/moviesReducer.js
+++ src/reducers/moviesReducer.js
@@ -16,3 +16,12 @@ export const hasErrored = (state = '', action) => {
       return state;
   }
 };
+
+export const movies = (state = [], action) => {
+  switch (action.type) {
+    case 'MOVIES_FETCH_DATA_SUCCESS':
+      return action.movies;
+    default:
+      return state;
+  }
+}
