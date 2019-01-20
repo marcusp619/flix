ecf6733247a0f1a864f65ea1929e279febadd485
marcusp619 <marcusp619@gmail.com>
2019-01-19 19:10:11 -0700

    Update vars to const

--- server-graphql/server.js
+++ server-graphql/server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var cors = require('cors');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const cors = require('cors');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 const schema = require('./schema');
 
-var root = { hello: () => 'Hello world!' };
+const app = express();
 
-var app = express();
 app.use('/graphql', cors(), graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
